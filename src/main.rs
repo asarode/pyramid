b9530563855035a6b4cdee1a1fd10147a7ee22f2
Arjun Sarode <sarodearjun57@gmail.com>
2017-06-14 11:26:08 -0700

    print out passed --input file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+extern crate clap;
+
+use clap::{Arg, App};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("Pyramid")
+        .version("1.0")
+        .author("Arjun Sarode <sarodearjun57@gmail.com>")
+        .about("Outputs an image pyramid from an input image.")
+        .arg(Arg::with_name("input")
+            .short("i")
+            .long("input")
+            .value_name("IMAGE_FILE")
+            .help("Sets the input file to use")
+            .takes_value(true)
+            .required(true))
+        .get_matches();
+
+    let input_file = matches.value_of("input").unwrap();
+    println!("Input file: {}", input_file);
 }
